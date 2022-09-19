d577b9ed1bccbb70883e4c9238d48711c4d71eb2
tovach <red5.dalv@gmail.com>
2022-09-19 18:58:19 +0300

    feat: add blueprint ui kit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 
+import { Button } from '@blueprintjs/core';
+
 import '@app/styles/index.css';
 
 export const App: FC = () => (
   <>
-    <div>Heloo</div>
+    <Button>Heloo</Button>
     <div>Heloo</div>
     <div>Heloo</div>
   </>
